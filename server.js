673cc6f8bad593b5ae3fe7b0268564a37b46179b
vikramhankare <30934218+vikramhankare@users.noreply.github.com>
2017-08-27 07:12:37 +0000

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,11 +9,11 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 var counter=0;
-app.get('/counter', function( req,res ){
+app.get('/counter', function(req, res) {
     counter=counter+1;
     res.send(counter.toString());
 });
-}
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
